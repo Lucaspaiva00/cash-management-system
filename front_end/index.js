abf838ac248aec26af2d4be872d229add56ffbd0
Lucaspaiva00 <paival907@gmail.com>
2024-07-16 22:20:53 -0300

    Atualização

--- front_end/index.js
+++ front_end/index.js
@@ -1,46 +1,37 @@
-const entradas = document.querySelector("#entradas");
-const saidas = document.querySelector("#saidas");
-const saldo = document.querySelector("#saldo");
-const msg = document.querySelector("#msg")
+const saldoEntradas = document.querySelector("#saldoEntradas");
+const saldoSaidas = document.querySelector("#saldoSaidas");
 const totalprop = document.querySelector("#totalprop");
-var saldoEntrada = 0;
-var saldoSaida = 0;
+const msg = document.querySelector("#msg");
 const uri = "http://localhost:3000";
 
 fetch("http://localhost:3000/caixa")
     .then(resp => resp.json())
     .then(resp => {
+        let saldoEntradas = 0;
+        let saldoSaidas = 0;
         resp.forEach(e => {
-            let dados =
-                `
-            <tr>
-                <td>${e.id}</td>
-                <td>${e.dataOperacao}</td>
-                <td>${e.tipoOperacao}</td>
-                <td>${e.meioPagamento}</td>
-                <td>${e.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-                <td>
-                <button type="button" title="button" class='btn btn-primary' id='editaroperacao' onClick='editaroperacao(${e.id})'>Editar</button>
-                <button type="button" title="button" class='btn btn-primary' id='excluirPerfil' onClick='excluirPerfil(${e.id})'>Excluir</button></td>
-            <tr>
-            `;
-            
             if (e.tipoOperacao == "Entrada") {
-                entradas.innerHTML += dados;
-                saldoEntrada += e.valor;
-            } else {
-                saidas.innerHTML += dados;
-                saldoSaida += e.valor;
+                saldoEntradas += e.valor;
+                document.querySelector("#saldoEntradas").innerHTML = saldoEntradas.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            }else if(e.tipoOperacao == "Saídas"){
+                saldoSaidas += e.valor;
+                document.querySelector("#saldoSaidas").innerHTML = saldoEntradas.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            }else{
+                console.log("Deu ruim");
             }
+        })
+    })
+
+// PUXANDO TOTAL EM PROPOSTAS
+fetch("http://localhost:3000/proposta")
+    .then(resp => resp.json())
+    .then(resp => {
+        let totalprop = 0;
+        resp.forEach(e => {
+            totalprop += e.valorProposta;
+            document.querySelector("#totalprop").innerHTML = totalprop.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
         });
-        document.querySelector("#saldoEntradas").innerHTML = saldoEntrada.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-        document.querySelector("#saldoSaidas").innerHTML = saldoSaida.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });;
-
-        document.querySelector("#saldo").innerHTML = (saldoEntrada - saldoSaida).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-
-    });
-
-
+    })
 
 //Funções CRUD - DELETE
 function excluirPerfil(id) {
@@ -63,44 +54,6 @@ function excluirPerfil(id) {
             });
     }
 }
-
-
-// PUXANDO LISTA DE PRODUTOS
-fetch("http://localhost:3000/produtos")
-    .then(resp => resp.json())
-    .then(resp => {
-        resp.forEach(e => {
-            produtosCadastrados.innerHTML +=
-                `
-        <tr>
-            <td>${e.id}</td>
-            <td>${e.nomeProduto}</td>
-            <td>${e.quantidadeProduto}</td>
-            <td>${e.valorProduto.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
-            <td>${e.statusProduto}</td>
-        <tr>
-        `
-        });
-
-    });
-
-
-
-
-// PUXANDO TOTAL EM PROPOSTAS
-fetch("http://localhost:3000/proposta")
-    .then(resp => resp.json())
-    .then(resp => {
-        let totalprop = 0;
-        resp.forEach(e => {
-            totalprop += e.valorProposta;
-            document.querySelector("#totalprop").innerHTML = totalprop.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-        });
-    })
-
-
-
-
 
 
 
