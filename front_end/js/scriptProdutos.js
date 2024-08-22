b6ee1293bf83e963b6bb1918e4c3f62f80f3b3f8
Lucaspaiva00 <paival907@gmail.com>
2024-08-22 13:21:59 -0300

    att

--- front_end/js/scriptProdutos.js
+++ front_end/js/scriptProdutos.js
@@ -23,8 +23,8 @@ fetch("http://localhost:3000/produtos")
             <td>${(e.valorProduto - e.custoProduto).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
             <td>${e.statusProduto}</td>
             <td>                
-            <button onclick="del('${e.id}')">[ - ]</button>
-                        <button onclick="edit(this)">[ * ]</button>
+            <button type="button" title="button" class='btn btn-primary' id='editaroperacao' onClick='editaroperacao(this)'>Editar</button>
+                <button type="button" title="button" class='btn btn-primary' id='excluirPerfil' onClick='excluirPerfil(${e.id})'>Excluir</button></td>
             </td>
         <tr>
         `;
@@ -37,7 +37,7 @@ fetch("http://localhost:3000/produtos")
 
     });
 
-function del(id) {
+function excluirPerfil(id) {
     if (confirm(`Confirma a exclusão do seu Cliente?`)) {
         fetch(uri + "/produtos/" + id, { method: "DELETE" })
             .then((resp) => {
@@ -57,92 +57,3 @@ function del(id) {
     }
 }
 
-
-function edit(id) {
-    let btn = event.target;
-    let linha = btn.parentNode.parentNode;
-    let celulas = linha.cells;
-
-    celulas[1].setAttribute('contenteditable', true);
-    celulas[2].setAttribute('contenteditable', true);
-    celulas[3].setAttribute('contenteditable', true);
-    celulas[4].setAttribute('contenteditable', true);
-    celulas[5].setAttribute('contenteditable', true);
-    celulas[6].setAttribute('contenteditable', true);
-    celulas[7].setAttribute('contenteditable', true);
-    celulas[8].innerHTML = `<button onclick="edit('${celulas[0].innerHTML}')">[ Salvar ]</button>`;
-
-    celulas[1].addEventListener('blur', function () {
-        celulas[1].removeAttribute('contenteditable');
-        celulas[2].removeAttribute('contenteditable');
-        celulas[3].removeAttribute('contenteditable');
-        celulas[4].removeAttribute('contenteditable');
-        celulas[5].removeAttribute('contenteditable');
-        celulas[6].removeAttribute('contenteditable');
-        celulas[7].removeAttribute('contenteditable');
-        celulas[8].innerHTML = `<button onclick="edit('${celulas[0].innerHTML}')">[ * ]</button>`;
-        fetch(uri + "/produtos/" + celulas[0].innerHTML, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                nomeProduto: celulas[1].innerHTML,
-                descricaoProduto: celulas[2].innerHTML,
-                quantidadeProduto: parseInt(celulas[3].innerHTML),
-                valorProduto: parseFloat(celulas[4].innerHTML.
-                    replace(',', '.')),
-                custoProduto: parseFloat(celulas[5].innerHTML.
-                    replace(',', '.')),
-                statusProduto: celulas[8].innerHTML
-                // statusProduto: celulas[8].innerHTML
-            })
-        })
-    })
-
-
-
-
-    // fetch("/produtos/" + id, { method: 'POST' })
-    //     .then(resp => {
-    //         if (resp.status != 200) {
-    //             return {
-    //                 error: "Erro ao editar o Produto",
-    //             };
-    //         } else return resp.json();
-    //     })
-    //     .then(resp => {
-    //         if (resp.error == undefined) {
-    //             console.log(resp);
-    //             window.location.reload();
-    //             btn.innerHTML = "[ * ]";
-    //             celulas[1].removeAttribute('contenteditable');
-    //             celulas[2].removeAttribute('contenteditable');
-    //             celulas[3].removeAttribute('contenteditable');
-    //             celulas[4].removeAttribute('contenteditable');
-    //             celulas[5].removeAttribute('contenteditable');
-    //             celulas[6].removeAttribute('contenteditable');
-    //             celulas[7].removeAttribute('contenteditable');
-    //         } else {
-    //             console.log(resp.error);
-    //         }
-    //     })
-
-
-    //     if (resp.status != 200) {
-    //         return {
-    //             error: "Erro ao editar o Produto",
-    //         };
-    //     } else return resp.json();
-
-    // })
-    // .then(resp => {
-    //     if (resp.error == undefined) {
-    //         console.log(resp);
-
-    //     } else {
-    //         console.log(resp.error);
-    //     }
-    // });
-
-}
