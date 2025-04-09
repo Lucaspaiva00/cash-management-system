533580b7565446d3998ff1947bb3f50a01521e65
lucaspaiva00 <paival907@gmail.com>
2025-04-09 13:00:14 -0300

    sistema

---
+++ api/src/controller/controlecaixa.js
@@ -0,0 +1,48 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const create = async (req, res) => {
+    try {
+        const caixa = await prisma.gerenciamentocaixa.create({
+            data: req.body
+        })
+        res.redirect("http://127.0.0.1:5500/web/index.html")
+    } catch (error) {
+        res.redirect("http://127.0.0.1:5500/web/erro.html")
+    }
+}
+
+const read = async (req, res) => {
+    const caixa = await prisma.gerenciamentocaixa.findMany()
+    return res.json(caixa)
+}
+
+const update = async (req, res) => {
+    try {
+        const caixa = await prisma.gerenciamentocaixa.update({
+            where: { id: parseInt(req.params.id) },
+            data: req.body
+        })
+        return res.status(202).json(caixa);
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+}
+
+const remove = async (req, res) => {
+    try {
+        const caixa = await prisma.gerenciamentocaixa.delete({
+            where: { id: parseInt(req.params.id) }
+        })
+        return res.status(204).json(caixa).end();
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+}
+
+module.exports = {
+    create,
+    read,
+    update,
+    remove
+}
