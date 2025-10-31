4307e86f5fbb9c13ee80c30be1daa44c44b19d41
lucaspaiva00 <paival907@gmail.com>
2025-10-31 15:16:56 -0300

    att

--- web/js/pdvvenda.js
+++ web/js/pdvvenda.js
@@ -1,4 +1,4 @@
-const BASE = "https://cash-management-system.onrender.com/vendas";
+const BASE = "https://cash-management-system.onrender.com";
 const usuario = JSON.parse(localStorage.getItem("usuarioLogado"));
 if (!usuario || !usuario.empresaId) {
     alert("Sessão expirada. Faça login novamente.");
