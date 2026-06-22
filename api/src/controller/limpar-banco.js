4f24df171075e902adee67861777547d6478a71b
Lucas Paiva <paival907@gmail.com>
2026-06-22 10:24:13 -0300

    att

--- api/src/controller/limpar-banco.js
+++ api/src/controller/limpar-banco.js
@@ -1,25 +1,44 @@
 const { PrismaClient } = require("@prisma/client");
-
 const prisma = new PrismaClient();
 
 const limparBanco = async (req, res) => {
     try {
+
+        await prisma.vendaItem.deleteMany();
         await prisma.venda.deleteMany();
+
+        await prisma.caixa.deleteMany();
+
+        await prisma.agenda.deleteMany();
+
         await prisma.proposta.deleteMany();
+
         await prisma.produto.deleteMany();
+
         await prisma.cliente.deleteMany();
+
+        await prisma.configuracaoFiscal.deleteMany();
+
+        await prisma.categoriaFinanceira.deleteMany();
+
+        await prisma.centroCusto.deleteMany();
+
         await prisma.usuario.deleteMany();
 
+        await prisma.empresa.deleteMany();
+
         return res.status(200).json({
-            message: "Banco de dados limpo com sucesso!"
+            message: "Banco limpo com sucesso!"
         });
+
     } catch (error) {
         console.error(error);
+
         return res.status(500).json({
-            error: "Erro ao limpar o banco de dados."
+            error: error.message
         });
     }
-}
+};
 
 module.exports = {
     limparBanco
